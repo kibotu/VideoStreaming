29cb8a43a1b5b38539a81ee45c48b6f0bc0e0aec
Jan Rabe <cloudgazer3d@gmail.com>
2016-03-26 18:20:48 +0100

    added readme to unity client

---
+++ UnityClient/Assets/Scripts/StreamLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreamLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityClient/README.md
@@ -0,0 +1,8 @@
+Unity client for video streams
+------------------------------
+
+Tiny client that will load a video stream by url.
+
+Requirements:
+
+[https://www.assetstore.unity3d.com/en/#!/content/10032](Easy Movie Texture)
