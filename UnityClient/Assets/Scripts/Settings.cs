ed9371610d8ac182032c968db356b0150b148f99
Jan Rabe <cloudgazer3d@gmail.com>
2016-03-27 14:02:07 +0200

    changed quality settings

---
+++ UnityClient/Assets/Scripts/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+  public class Settings : MonoBehaviour
+  {
+    /// http://answers.unity3d.com/questions/545637/graphics-quality-settings-on-android.html
+    [Range(0, 5)] [SerializeField] private int _quality;
+
+    void Update()
+    {
+      QualitySettings.SetQualityLevel(_quality, true);
+    }
+  }
+}
